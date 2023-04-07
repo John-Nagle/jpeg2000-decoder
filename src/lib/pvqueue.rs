13463b6aa36323c2933703239057236f4c1c567b
John Nagle <nagle@sitetruth.com>
2023-04-07 10:42:33 -0700

    Bottleneck test passed

--- src/lib/pvqueue.rs
+++ src/lib/pvqueue.rs
@@ -80,17 +80,23 @@ fn testpvqueuebasic() {
 }
 
 #[test]
+/// Run N threads and make sure no more than BOTTLENECK_COUNT are in the bottleneck section.
 fn testpvqueuethreaded() {
     const WORKER_COUNT: usize = 10;
     const BOTTLENECK_COUNT: u32 = 3;
     let queue = PvQueue::new(BOTTLENECK_COUNT);
     let mut workers = Vec::new();
+    let active = Arc::new(std::sync::atomic::AtomicU32::new(0)); // number of active workers
     for n in 0..WORKER_COUNT {
         let queue_clone = Arc::clone(&queue);
+        let active_clone = Arc::clone(&active);
         workers.push(std::thread::spawn(move || {
             {   let _locked = PvQueue::lock(&queue_clone); // limit the number of threads active simultaneously
-                println!("Thread #{} start.", n);
+                let cnt = active_clone.fetch_add(1, std::sync::atomic::Ordering::Relaxed);    // count of others active.
+                println!("Thread #{} start, {} actives.", n, cnt + 1);
+                assert!(cnt < BOTTLENECK_COUNT);    // must not have too many actives inside the bottleneck.
                 std::thread::sleep_ms(100); // stall;
+                let _ = active_clone.fetch_sub(1, std::sync::atomic::Ordering::Relaxed);
                 println!("Thread #{} done.", n);
             }         
         }))
